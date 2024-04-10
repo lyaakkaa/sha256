dde7936b15ce00c2ccfdc714a9ad3987b6249db3
akmiii <akmira2410@mail.ru>
2024-04-10 19:54:30 +0600

    host and port

---
+++ server.py
@@ -0,0 +1,10 @@
+import socket
+import threading
+
+HOST = '127.0.0.1'
+PORT = 55555
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+server.listen()
+
+clients = []
