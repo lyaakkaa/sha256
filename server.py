09ca7bcfc2f41a11d354cb0e7fbc9e89dbc691c2
lyaakkaa <leila.bekturgan@gmail.com>
2024-04-17 12:49:00 +0500

    server done

--- server.py
+++ server.py
@@ -8,3 +8,30 @@ server.bind((HOST, PORT))
 server.listen()
 
 clients = []
+
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            print(f"Received encrypted message: {message}")
+            broadcast(message)
+        except:
+            clients.remove(client)
+            client.close()
+            break
+
+def receive():
+    while True:
+        client, address = server.accept()
+        print(f"Connected with {str(address)}")
+        clients.append(client)
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+print("Server is listening...")
+receive()
